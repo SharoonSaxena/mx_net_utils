16ed91fd38581a4504b4bb03f5e984eb901ab484
Sharoon Saxena <charismaticsharoon@gmail.com>
2020-08-12 12:47:25 +0530

    added synthetic data and try_gpu

---
+++ my_mx_util.py
@@ -0,0 +1,28 @@
+# This file contains utility functions to make life easier
+import mxnet as mx
+from mxnet import np, npx
+npx.set_np()
+
+def try_gpu(i=0):
+    """
+    Return gpu(i) if it exists, else return cpu()
+    """
+    return npx.gpu(i) if npx.num_gpus()>=i+1 else npx.cpu()
+
+
+def synthetic_data(w, b, num_examples):
+    """[summary]
+
+    Args:
+        w (list): list of weights for independent variables
+        b (int): some value for bias
+        num_examples (int): number of observations
+
+    Returns 
+    X, Y (independent data, target data): synthetic data and target data
+    """
+
+    X = np.random.normal(0,1,(num_examples, len(w)))
+    y = np.dot(X, w) + b
+    y+= np.random.normal(0, 0.01, y.shape)
+    return X, y.reshape((-1,1))
